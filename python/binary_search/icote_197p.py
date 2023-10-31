9e5580ff46dcf1e9c6ae37a5cdf033c0c1c875c5
codeeon <yeon.develop@gmail.com>
2023-10-31 17:40:54 +0900

    leet_704 - 바이너리 서치(이진 검색)

---
+++ python/binary_search/icote_197p.py
@@ -0,0 +1,9 @@
+# 이것이 코딩 테스트다. 197p. 부품 찾기
+
+import sys
+
+N = input(int(sys.stdin.readline))
+a = input(int(sys.stdin.readline))
+M = input(int(sys.stdin.readline))
+b = input(int(sys.stdin.readline))
+
